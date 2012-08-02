4eb7e42aaed431206c771c6d71c7b133f073c1ce
Alexander Kuzmenok <kmz111@gmail.com>
2012-08-02 15:27:04 +0300

    Code checks improvement, readme formatted

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
+ready
+=========
+
 A cross-browser way of handling the document onReady event.
 
 Provides a way to set a function that will run after DOM is ready.
 
+# Examples
+
+A common way to use:
+
 ```
 ready(function() {
 	// we are ready
-	alert('ready!');
+	console.log('ready!');
+});
+```
+
+`ready` returns itself, so you can add another function right away:
+
+```
+ready(function(){
+   console.log('ready 1');     
+})(function() {
+   console.log('ready 2'); 
 });
 ```
--- ready.js
+++ ready.js
@@ -1,19 +1,20 @@
 var ready = function(f) {
 	if (!ready.r) ready.r = [];
+	if (!ready.t) ready.t = null;
 	ready.r.push(f);
 	var run = function() {
-		clearTimeout(t);
+		clearTimeout(ready.t);
 		while (ready.r.length)
 			ready.r.splice(0,1)[0]();
 	}
-	var t = null;
 	var check = function() {
 		if (document && document.getElementsByTagName && document.getElementById && document.body) {
 			run();
 		} else {
-			t = setTimeout(check, 13);
+			ready.t = setTimeout(check, 13);
 		}
 	}
-	check();
+	if (ready.t == null)
+		check();
 	return ready;
 };
